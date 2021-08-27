cf9a791b9f787a92bd33162aebf4e950ad0a036d
Tu Nguyen <im2nguyen@gmail.com>
2021-08-26 23:48:16 -0700

    update resource name

--- teams.tf
+++ teams.tf
@@ -10,7 +10,7 @@ resource "github_team" "all_teams" {
 }
 
 # This resource will add the currently authenticated user as a member of `my_team`
-resource "github_team_membership" "security_self" {
+resource "github_team_membership" "self" {
   for_each = github_team.all_teams
 
   team_id  = each.value.id
