959b7f371b35a94fd8b1c80fda272a7989f5c2aa
Tu Nguyen <im2nguyen@gmail.com>
2021-08-30 09:12:33 -0700

    Add default maintainer for teams

--- teams.tf
+++ teams.tf
@@ -4,18 +4,10 @@ resource "github_team" "all" {
     team.name => team
   }
 
-  name        = each.value.name
-  description = each.value.description
-  privacy     = each.value.privacy
-}
-
-# This resource will add the currently authenticated user as a member of `my_team`
-resource "github_team_membership" "self" {
-  for_each = github_team.all
-
-  team_id  = each.value.id
-  username = data.github_user.self.login
-  role     = "maintainer"
+  name                      = each.value.name
+  description               = each.value.description
+  privacy                   = each.value.privacy
+  create_default_maintainer = true
 }
 
 resource "github_team_membership" "members" {
